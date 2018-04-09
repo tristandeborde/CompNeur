d486d36a3a6cd87de9bb39bd6e5ccd0a8896d821
Tristan DEBORDE <tdeborde@e2r9p5.42.fr>
2018-04-09 17:51:56 +0200

    ex03 done, ex04 in progress

--- ex03/question9.py
+++ ex03/question9.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import spline
-
+import math
 
 with open('data.pickle', 'rb') as f:
     data = pickle.load(f)
@@ -13,26 +13,30 @@ with open('pop_coding.pickle', 'rb') as f:
 stim = data['stim']
 del data['stim']
 
-keys = list(data.keys())
+keys = list(sorted(data.keys()))
 
-max_rates = []
 #compute max_rate
+max_rates = []
 for neuron in keys:
     trans_neur = np.transpose(data[neuron])
     mfr = [np.mean(trans_neur[t]) for t in range(24)]
     max_rates.append([neuron, max(mfr)])
+max_rates = list(max_rates)
 
-max_rates = list(reversed(max_rates))
-#separate bas_vec and rates
+#separate bas_vec and rates in pop_coding data
 bas_vec = {k: pop_coding[k] for k in ('c1', 'c2', 'c3', 'c4')}
-bas_vec = [bas_vec[i] for i in sorted(bas_vec.keys())]
-for k in list(bas_vec.keys()):
+bas_vec_list = []
+for k in sorted(bas_vec.keys()):
     del pop_coding[k]
+    bas_vec_list.append(bas_vec[k]) 
+bas_vec = bas_vec_list
+
+#compute population vector
 it = 0
 vec_fi = [0, 0]
-print(bas_vec)
 for i in sorted(pop_coding.keys()):
-    vec_fi += (np.mean(pop_coding[i] / max_rates[it][1])) * bas_vec[it]
-    print(vec_fi)
-    #print(max_rates[it])
+    vec_fi += (np.mean(pop_coding[i]) / max_rates[it][1]) * bas_vec[it]
     it += 1;
+print(bas_vec)
+print(pop_coding)
+print(90 - math.degrees(math.atan2(vec_fi[1], vec_fi[0])))
