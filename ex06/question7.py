c95c4fbeaac366f1a4221d7e374c30ff13b9e4cf
Tristan DEBORDE <tdeborde@e2r8p5.42.fr>
2018-04-13 18:32:49 +0200

    ex06 done

--- ex06/question7.py
+++ ex06/question7.py
@@ -7,23 +7,28 @@ with open('c10p1.pickle', 'rb') as f:
     data = pickle.load(f)
 data = np.array(data['c10p1'])
 #normalize
-data -= np.mean(data, axis=0)
+mu = np.mean(data, axis=0)
+data -= mu
 
-cov_mat = np.dot(np.transpose(data), data) / 100
-lan, e = LA.eig(cov_mat)
-print (lan, '\n', e, '\n', cov_mat, '\n\n')
+
+corr_mat = np.dot(data.T, data) / 100
+lan, e = LA.eig(corr_mat)
+print (lan[0], '\n', e[:, 0], '\n', corr_mat, '\n\n')
 
 N = 1
-alpha = 1
+alpha = 1.0
 delta_t = 0.01
-w = [0.1222, 0.94]
-w_ev = []
+w = np.random.rand(2)
+print(type(w))
 
-for i in np.arange(0, 100000, 1):
-    v = data[i%100] * w
-    w = w + delta_t * N * (v*data[i%100] - alpha*(v**2)*w)
+for z in range (5):
+    for i in np.arange(0, 100000, 1):
+        v = np.dot(data[i%100, :], w)
+        w = w + delta_t * N * (v*data[i%100,:])# - alpha*v*v*w)
+    print('mu = {} --- v = {} --- w = {}\n'.format(np.mean(data, axis=0), v, w))
+    data[:,0] += z
+    data[:,1] += z * 1/4
     '''if (i%1000 == 0):
         w_ev.append(w) 
 w_ev = np.asarray(w_ev)
 '''
-print(v, w)
